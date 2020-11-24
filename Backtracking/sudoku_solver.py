7c3ea8ee5d14bb703edeff1bf98460397973cb75
Jason <jason8maharjan@gmail.com>
2020-11-24 23:04:01 +0545

    added files via commit

--- Backtracking/sudoku_solver.py
+++ Backtracking/sudoku_solver.py
@@ -1,139 +1,98 @@
-# Solve Sudoku using Backtracking
-import turtle
-from time import sleep
+# Sudoku solver using backtracking
+# Display grid
+def display_grid(grid):
+    print('\n')
+    for i in range(9):
+        for j in range(9):
+            print (grid[i][j], end = ' ')
+        print('\n')
 
-#initialise sodoku board
-grid = []
-grid.append([3, 0, 6, 5, 0, 8, 4, 0, 0])
-grid.append([5, 2, 0, 0, 0, 0, 0, 0, 0])
-grid.append([0, 8, 7, 0, 0, 0, 0, 3, 1])
-grid.append([0, 0, 3, 0, 1, 0, 0, 8, 0])
-grid.append([9, 0, 0, 8, 6, 3, 0, 0, 5])
-grid.append([0, 5, 0, 0, 9, 0, 6, 0, 0])
-grid.append([1, 3, 0, 0, 0, 0, 2, 5, 0])
-grid.append([0, 0, 0, 0, 0, 0, 0, 7, 4])
-grid.append([0, 0, 5, 2, 0, 6, 3, 0, 0])
+# Searches the grid to find an entry that is still unassigned. 
+# If found, the reference parameters row, col will be set to the grid cell that is unassigned, and true is returned.
+# If not found unassigned entries remains, false is returned.
+# 'l' is a list  variable that has been passed from the solve_sudoku function to keep track of incrementation of Rows and Columns
+def find_empty_cells(grid, l):
+    for row in range(9):
+        for col in range(9):
+            if(grid[row][col] == 0):
+                l[0]= row
+                l[1]= col
+                return 1
+    return 0
+ 
+# check if assigned entry in the specified row matches the given number
+def used_in_row(grid, row, num):
+    for i in range(9):
+        if(grid[row][i] == num):
+            return 1
+    return 0
+ 
+# check if assigned entry in the specified column matches the given number
+def used_in_col(grid, col, num):
+    for i in range(9):
+        if(grid[i][col] == num):
+            return 1
+    return 0
+ 
+# check if assigned entry within the specified 3x3 box matches the given number
+def used_in_box(grid, row, col, num):
+    for i in range(3):
+        for j in range(3):
+            if(grid[i + row][j + col] == num):
+                return 1
+    return 0
+ 
+# Check if a digit can be assigned to the given row, col
+def check_cell(grid, row, col, num):
+    # Check if 'num' is not already placed in current row,current column and current 3x3 box
+    return (not used_in_row(grid, row, num) and
+           not used_in_col(grid, col, num) and
+           not used_in_box(grid, row - row % 3, 
+                           col - col % 3, num))
+ 
+# Solve the partial grid fulfilling the 3 constraints
+def solve_sudoku(grid):
+    # 'l' is a list variable that keeps the record of row and col in find_empty_cells()
+    l =[0, 0]
+     
+    # If there is no unassigned location, solution is attained
+    if(not find_empty_cells(grid, l)):
+        return 1
+     
+    # Assigning list values to row and col that we got from the above Function 
+    row = l[0]
+    col = l[1] 
 
-# Draw board on the screen
-myPen = turtle.Turtle()
-myPen.hideturtle()
-myPen.tracer(0)
-myPen.speed(0)
-myPen.pencolor("green")
-myPen.fillcolor("black")
-topLeft_x = -150
-topLeft_y = 150
-
-# Draw text on the screen
-def text(message,x,y,size):
-    myPen.penup()
-    myPen.goto(x,y)    		  
-    myPen.write(message, align="left", font = ('Comic Sans MS', size, 'normal'))
-
-# Draw grid on the screen
-def drawGrid(grid):
-  intDim = 35
-  for row in range(0,10):
-    if (row%3)==0:
-      myPen.pensize(3)
+    # for digits 1 to 9
+    for num in range(1, 10):
+        if(check_cell(grid, row, col, num)):
+            # make tentative assignment
+            grid[row][col]= num
+            # return, if success
+            if(solve_sudoku(grid)):
+              return 1
+ 
+            # failure, unmake & try again
+            grid[row][col] = 0
+             
+    # trigger backtracking       
+    return 0
+ 
+# Driver funtion
+if __name__=="__main__":
+     
+    grid = []
+    grid.append([3, 0, 6, 5, 0, 8, 4, 0, 0])
+    grid.append([5, 2, 0, 0, 0, 0, 0, 0, 0])
+    grid.append([0, 8, 7, 0, 0, 0, 0, 3, 1])
+    grid.append([0, 0, 3, 0, 1, 0, 0, 8, 0])
+    grid.append([9, 0, 0, 8, 6, 3, 0, 0, 5])
+    grid.append([0, 5, 0, 0, 9, 0, 6, 0, 0])
+    grid.append([1, 3, 0, 0, 0, 0, 2, 5, 0])
+    grid.append([0, 0, 0, 0, 0, 0, 0, 7, 4])
+    grid.append([0, 0, 5, 2, 0, 6, 3, 0, 0])
+     
+    if(solve_sudoku(grid)):
+        display_grid(grid)
     else:
-      myPen.pensize(1)
-    myPen.penup()
-    myPen.goto(topLeft_x,topLeft_y-row*intDim)
-    myPen.pendown()
-    myPen.goto(topLeft_x+9*intDim,topLeft_y-row*intDim)
-  for col in range(0,10):
-    if (col%3)==0:
-      myPen.pensize(3)
-    else:
-      myPen.pensize(1)    
-    myPen.penup()
-    myPen.goto(topLeft_x+col*intDim,topLeft_y)
-    myPen.pendown()
-    myPen.goto(topLeft_x+col*intDim,topLeft_y-9*intDim)
-
-  for row in range (0,9):
-      for col in range (0,9):
-        # draw text of non zero cells
-        if grid[row][col]!=0:
-          text(grid[row][col], topLeft_x+col*intDim+9, topLeft_y-row*intDim-intDim+8, 15)
-
-
-# Check if the grid is full
-def checkGrid(grid):
-  for row in range(0,9):
-      for col in range(0,9):
-        # check if cell is empty 
-        if grid[row][col] == 0:
-          return False
-
-  # no empty cells
-  return True 
-
-# Backtracking Algorithm
-def solveGrid(grid):
-  # Find and fill empty cells
-  for i in range(0,81):
-    row = i // 9
-    col = i % 9
-    if grid[row][col] == 0:
-      for value in range (1,10):
-        # Check if value already exists in this row
-        if not(value in grid[row]):
-          # Check if value already exists in this column
-          if not value in (grid[i][col] for i in range(0,9)):
-            # current square
-            square=[]
-            if row<3:
-              if col<3:
-                square=[grid[i][0:3] for i in range(0,3)]
-              elif col<6:
-                square=[grid[i][3:6] for i in range(0,3)]
-              else:  
-                square=[grid[i][6:9] for i in range(0,3)]
-            elif row<6:
-              if col<3:
-                square=[grid[i][0:3] for i in range(3,6)]
-              elif col<6:
-                square=[grid[i][3:6] for i in range(3,6)]
-              else:  
-                square=[grid[i][6:9] for i in range(3,6)]
-            else:
-              if col<3:
-                square=[grid[i][0:3] for i in range(6,9)]
-              elif col<6:
-                square=[grid[i][3:6] for i in range(6,9)]
-              else:  
-                square=[grid[i][6:9] for i in range(6,9)]
-                
-            # Check if value isn't already used on this 3x3 square
-            if not value in (square[0] + square[1] + square[2]):
-              grid[row][col] = value # place the value in the current cell
-              myPen.clear() # erase previous values
-              drawGrid(grid) 
-              myPen.getscreen().update()     
-              
-              # Check if grid is completed and checked
-              if checkGrid(grid):
-                print("Grid Complete and Checked")
-                return True
-              else:
-                if solveGrid(grid):
-                  return True
-      break
-  grid[row][col] = 0
-  
-  
-drawGrid(grid) 
-myPen.getscreen().update()
-sleep(1)
-
-solved = solveGrid(grid)
-if solved:
-  print("Sudoku Solved")
-  text("Sudoku Solved",-60,-190,15)
-else:  
-  print("Cannot Solve Sudoku Grid")
-  text("Cannot Solve Sudoku Grid",-130,-190,20)
-
-myPen.getscreen().update()	
+        print ("Solution doesn't exist for the given sudoku")
---
+++ Backtracking/sudoku_solverr.py
@@ -0,0 +1,86 @@
+def displayGrid(grid):
+   print('\n')
+   for i in range(9):
+      for j in range(9):
+         print (grid[i][j], end = ' ')
+      print('\n')
+
+# Check if the grid is full
+def checkGrid_full(grid):
+  # loop through each row
+  for row in range(0,9):
+    # loop through every column in very row
+      for col in range(0,9):
+        # check if cell is empty
+        if grid[row][col] == 0:
+          return 0
+  # Grid is complete
+  return 1
+ 
+def solveGrid(grid):
+   for i in range(0,81):
+      row = i // 9
+      col = i % 9
+      # look for empty cell to put digits(1-9)
+      if grid[row][col] == 0:
+         for val in range (1,10):
+            # Check if digit is already used in this row
+            if not(val in grid[row]):
+               # Check if digit is already used in this column
+               if not(val in (grid[i][col] for i in range(9))):
+                  # current square (out of the 9 squares)
+                  current_square = []
+                  if row<3:
+                     if col<3:
+                        current_square=[grid[i][0:3] for i in range(0,3)]
+                     elif col<6:
+                        current_square=[grid[i][3:6] for i in range(0,3)]
+                     else:  
+                        current_square=[grid[i][6:9] for i in range(0,3)]
+                  elif row<6:
+                     if col<3:
+                        current_square=[grid[i][0:3] for i in range(3,6)]
+                     elif col<6:
+                        current_square=[grid[i][3:6] for i in range(3,6)]
+                     else:  
+                        current_square=[grid[i][6:9] for i in range(3,6)]
+                  else:
+                     if col<3:
+                        current_square=[grid[i][0:3] for i in range(6,9)]
+                     elif col<6:
+                        current_square=[grid[i][3:6] for i in range(6,9)]
+                     else:  
+                        current_square=[grid[i][6:9] for i in range(6,9)]
+
+                  # Check if this value has not already been used on the 3x3 current_square
+                  if not val in (current_square[0] + current_square[1] + current_square[2]):
+                     grid[row][col] = val
+
+                     # Check if grid is full
+                     if checkGrid_full(grid):
+                        return 1
+                     else:
+                        if solveGrid(grid):
+                           return 1
+         break
+   # remove the assigned value i.e. backtracking is commenced
+   grid[row][col] = 0
+ 
+# Driver funtion
+if __name__=="__main__":
+   # initialize partial grid
+   grid = []
+   grid.append([3, 0, 6, 5, 0, 8, 4, 0, 0])
+   grid.append([5, 2, 0, 0, 0, 0, 0, 0, 0])
+   grid.append([0, 8, 7, 0, 0, 0, 0, 3, 1])
+   grid.append([0, 0, 3, 0, 1, 0, 0, 8, 0])
+   grid.append([9, 0, 0, 8, 6, 3, 0, 0, 5])
+   grid.append([0, 5, 0, 0, 9, 0, 6, 0, 0])
+   grid.append([1, 3, 0, 0, 0, 0, 2, 5, 0])
+   grid.append([0, 0, 0, 0, 0, 0, 0, 7, 4])
+   grid.append([0, 0, 5, 2, 0, 6, 3, 0, 0])     
+
+   if(solveGrid(grid)):
+      displayGrid(grid)
+   else:
+      print ("Solution doesn't exist for the given sudoku")
